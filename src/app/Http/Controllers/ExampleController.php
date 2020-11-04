d96f0956e4d96b550a206d8ea1ef838a64eb1b54
Osamah Khd <osqaw@cashu.com>
2020-11-04 11:14:55 +0200

    first commit

---
+++ src/app/Http/Controllers/ExampleController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+
+class ExampleController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Retrieve the user for the given ID.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        return User::findOrFail($id);
+    }
+}
