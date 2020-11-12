d654905dcc85dc5a5f45f341064d23e4339f3a5d
Osamah Khd <osqaw@cashu.com>
2020-11-12 19:57:12 +0200

    Add validations on the controller.

--- src/app/Http/Controllers/SendEmailController.php
+++ src/app/Http/Controllers/SendEmailController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Queue;
 use App\Jobs\EmailJob;
+use \Validator;
 
 class SendEmailController extends Controller
 {
@@ -31,7 +32,28 @@ class SendEmailController extends Controller
         $data = $request->json()->all();
         $recipients = $data['reciepents'];
         foreach ($recipients as $recipient) {
-            Queue::push(new EmailJob(array('operation' => 'register', 'first_name' => $recipient['first_name'], 'last_name' => $recipient['last_name'], 'email' => $recipient['email'], 'format' => $recipient['format'], 'priority' => 1)));
+            $first_name = $recipient['first_name'];
+            $last_name = $recipient['last_name'];
+            $email = $recipient['email'];
+            $format = $recipient['format'];
+
+            $validator = Validator::make([
+                'first_name' => $first_name,
+                'last_name' => $last_name,
+                'email' => $email,
+                'format' => $format,
+            ], [
+                'first_name' => ['required'],
+                'last_name' => ['required'],
+                'email' => ['required', 'email'],
+                'format' => ['in:html,text']
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(['error' => $validator->errors()->all()]);
+            }
+
+            Queue::push(new EmailJob(array('operation' => 'register', 'first_name' => $first_name, 'last_name' => $last_name, 'email' => $email, 'format' => $format, 'priority' => 1)));
         }
         // Log::info(print_r($recipient, true));
         return response()->json(['success' => true]);
@@ -48,7 +70,28 @@ class SendEmailController extends Controller
         $data = $request->json()->all();
         $recipients = $data['reciepents'];
         foreach ($recipients as $recipient) {
-            Queue::push(new EmailJob(array('operation' => 'forgetPassword', 'first_name' => $recipient['first_name'], 'last_name' => $recipient['last_name'], 'email' => $recipient['email'], 'format' => $recipient['format'], 'priority' => 1)));
+            $first_name = $recipient['first_name'];
+            $last_name = $recipient['last_name'];
+            $email = $recipient['email'];
+            $format = $recipient['format'];
+
+            $validator = Validator::make([
+                'first_name' => $first_name,
+                'last_name' => $last_name,
+                'email' => $email,
+                'format' => $format,
+            ], [
+                'first_name' => ['required'],
+                'last_name' => ['required'],
+                'email' => ['required', 'email'],
+                'format' => ['in:html,text']
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(['error' => $validator->errors()->all()]);
+            }
+
+            Queue::push(new EmailJob(array('operation' => 'forgetPassword', 'first_name' => $first_name, 'last_name' => $last_name, 'email' => $email, 'format' => $format, 'priority' => 1)));
         }
         // Log::info(print_r($recipient, true));
         return response()->json(['success' => true]);
